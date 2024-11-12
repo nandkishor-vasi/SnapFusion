f850c9c73277ebf0381f1143ac2d4accbe859cd8
nandkishor vasi <nandkishorvasi@gmail.com>
2024-11-12 14:36:10 +0530

    authentication_01

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,12 +1,15 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
     namespace = "com.example.snapfusion2"
     compileSdk = 35
-
+buildFeatures {
+    viewBinding = true
+}
     defaultConfig {
         applicationId = "com.example.snapfusion2"
         minSdk = 24
@@ -42,8 +45,11 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("de.hdodenhof:circleimageview:3.1.0")
+    // Import the BoM for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
 }
--- app/src/main/java/com/example/snapfusion2/SignupActivity.kt
+++ app/src/main/java/com/example/snapfusion2/SignupActivity.kt
@@ -1,20 +1,35 @@
 package com.example.snapfusion2
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.snapfusion2.databinding.ActivitySignupBinding
 
 class SignupActivity : AppCompatActivity() {
+    val binding by lazy {
+        ActivitySignupBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_signup)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        binding.signUpBtn.setOnClickListener {
+            if(binding.name.editText?.text.toString().equals("") or
+                binding.email.editText?.text.toString().equals("") or
+                binding.password.editText?.text.toString().equals(""))
+            {
+                Toast.makeText(this@SignupActivity, "Please fill the information", Toast.LENGTH_SHORT).show()
+            } else {
+
+            }
         }
     }
 }
