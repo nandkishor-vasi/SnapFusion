59099f260f0de9112706ed9c4f4513c5b6674e68
nandkishor vasi <nandkishorvasi@gmail.com>
2024-11-12 16:20:17 +0530

    authentication_02

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,8 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
+    implementation(libs.firebase.firestore.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
